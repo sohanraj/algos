d20532191570a5ecf8f9cbe9cb8901659927a25b
sohanraja@gmail.com <sohanraja@gmail.com>
2024-10-10 17:06:00 -0400

    added algo highest power of 2 that divides the number n - added comments

--- basic_algorithms/highest_power_of_2_that_divide_n.py
+++ basic_algorithms/highest_power_of_2_that_divide_n.py
@@ -1,8 +1,12 @@
 def solution(N):
     # Implement your solution here
+    # checks if  number is divisible by 2
     if N % 2 == 0:
+        # loop starts
         for i in range(N):
+            # variable start from hight possible value
             k = N - i
+            # checks highest power to 2 is divisible
             if N % (2**k) == 0:
                 return k
             i = i + 1
